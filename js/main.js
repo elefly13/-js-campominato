223d64b4a52f6e81502ee5535b4a47eda08b99ca
Elefly13 <elymara13@gmail.com>
2021-07-27 18:36:47 +0200

    fine esercizio

--- js/main.js
+++ js/main.js
@@ -32,42 +32,42 @@ console.log(myArrayBombe.sort());
 
 
 // In seguito deve chiedere all’utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
-
-let arrayNumGiocatore = [];
-let numeroGiocatore = 0;
-let bombaTrovata = false;
-let possibilita = 100 - numBombe;
-do {
-    let numeroGiocatore = parseInt(prompt("Inserisci un numero tra 1 e 100"));
+// let arrayNumGiocatore = [];
+// let numeroGiocatore = 0;
+// let bombaTrovata = false;
+// let possibilita = 100 - numBombe;
+// do {
+//     let numeroGiocatore = parseInt(prompt("Inserisci un numero tra 1 e 100"));
     
-    // verifica se il numero giocatore è uguale al numero bomba
-    if (myArrayBombe.includes(numeroGiocatore)) {
-        bombaTrovata = true;
-    } else {
-        // l'utente non può inserire due volte lo stesso numero
-        if (existInArray(arrayNumGiocatore, numeroGiocatore) == false) {
-            arrayNumGiocatore.push(numeroGiocatore);
-        }  
-    }
- // Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all’utente un altro numero.       
-} while ((arrayNumGiocatore.length < possibilita) && (bombaTrovata == false)); 
-// La partita termina quando il giocatore inserisce un numero “vietato”
-if (bombaTrovata = true) {
-    alert("bomba esplosa hai perso");
-}
-// o raggiunge il numero massimo possibile di numeri consentiti.
-else if (possibilita = 100 - numBombe) {
-    console.log("sei una bomba hai vinto");
-}
-// Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
-alert(" Hai totalizzato punti: " + arrayNumGiocatore.length);
+//     // verifica se il numero giocatore è uguale al numero bomba
+//     if (myArrayBombe.includes(numeroGiocatore)) {
+//         bombaTrovata = true;
+//     } else {
+//         // l'utente non può inserire due volte lo stesso numero
+//         if (existInArray(arrayNumGiocatore, numeroGiocatore) == false) {
+//             arrayNumGiocatore.push(numeroGiocatore);
+//         }  
+//     }
+//  // Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all’utente un altro numero.       
+// } while ((arrayNumGiocatore.length < possibilita) && (bombaTrovata == false)); 
+
+// // La partita termina quando il giocatore inserisce un numero “vietato”
+// if (bombaTrovata = true) {
+//     alert("bomba esplosa hai perso");
+// }
+// // o raggiunge il numero massimo possibile di numeri consentiti.
+// else if (possibilita = 100 - numBombe) {
+//     console.log("sei una bomba hai vinto");
+// }
+// // Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
+// alert(" Hai totalizzato punti: " + arrayNumGiocatore.length);
 
 // fine sezione logica gioco 
 
 // sezione funzioni per la grafica
 
 function creaCampo(celle) {
-    for (let i = 2; i <= celle; i++) {
+    for (let i = 1; i <= celle; i++) {
         let cella = `
              <div data-cella = ${i} class="cella"></div>
         `;
@@ -79,14 +79,51 @@ function creaCampo(celle) {
 
     }
 }
-
+let arrayNumGiocatore = [];
 document.getElementById('campo').addEventListener('click',
 
-    function (e) { //e = event
+    (e) => { //e = event
         console.log(e.target.dataset.cella);
         let element = document.querySelectorAll("[data-cella='" + e.target.dataset.cella + "']");
         console.log(element[0]);
-        element[0].classList.add("red");
+        // collego il mio array bombe alle celle 
+        if ( existInArray(myArrayBombe, e.target.dataset.cella) == false) {
+            element[0].classList.add("green");
+        } else {
+            element[0].classList.add("red");
+        }
+
+        
+        let bombaTrovata = false;
+        let possibilita = 100 - numBombe;
+        
+        let numeroGiocatore = parseInt(e.target.dataset.cella);
+
+        console.log(arrayNumGiocatore);
+                
+        // verifica se il numero giocatore è uguale al numero bomba
+        if (myArrayBombe.includes(numeroGiocatore)) {
+            bombaTrovata = true;
+        } else {
+            // l'utente non può inserire due volte lo stesso numero
+            if (existInArray(arrayNumGiocatore, numeroGiocatore) == false) {
+            arrayNumGiocatore.push(numeroGiocatore);
+            }    
+        } 
+        
+        // Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all’utente un altro numero.    
+        // La partita termina quando il giocatore inserisce un numero “vietato”
+        if (bombaTrovata == true) {
+            alert("bomba esplosa hai perso");
+             // Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
+            alert(" Hai totalizzato punti: " + arrayNumGiocatore.length);
+        }
+        // o raggiunge il numero massimo possibile di numeri consentiti.
+        else if (possibilita == arrayNumGiocatore.length) {
+            alert("sei una bomba hai vinto");
+        }
+       
+
     }
 )
 
